5808e45e4e81240a12896ba733d5eaaed79bd629
Djb <contact@psd-html.fr>
2016-02-02 15:14:44 +0100

    Update Tweetie.php

--- Tweetie/Tweetie.php
+++ Tweetie/Tweetie.php
@@ -40,23 +40,18 @@ class Tweetie extends plxPlugin {
         <script src="<?php echo PLX_PLUGINS ?>Tweetie/app/tweetie.js"></script>
 
         <script>
-                $(document).ready(function() {
+                $(document).ready(function($) {
                 $('#tweecool').tweecool({
                     // Your twitter username
                     username : '<?php echo $this->getParam('username');?>',
-
                     // Number of tweets to show
                     limit : <?php echo $this->getParam('nb');?>, 
-
                     // Show profile image
                     profile_image : <?php echo $this->getParam('avatar');?>, 
-
                     // Show tweet time
                     show_time : <?php echo $this->getParam('time');?>, 
-
                     // Show media
                     show_media : <?php echo $this->getParam('media');?>,
-
                     //values: small, large, thumb, medium 
                     show_media_size: '<?php echo $this->getParam('media_size');?>'  
                     });
